dd5e47b22ad7516f86514a0112e7985a90cadf2d
WaiZawTun-coder <waizawtun.coder@gmail.com>
2026-02-18 00:39:48 +0630

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,17 @@
 import { AuthProvider } from "../context/AuthContext";
 import "./globals.css";
 
+export const metadata = {
+  title: "Yukai - Admin Dashboard",
+  description:
+    "Admin dashboard for managing users, content, and settings of the Yukai social media platform.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
